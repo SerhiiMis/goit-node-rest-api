47656a12614f6e887b53742978c547d08abfdb95
Serhii Mishovych <serhiimishovych@gmail.com>
2025-06-08 20:19:42 +0200

    Add Joi schemas for creating and updating contacts

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,5 +1,34 @@
 import Joi from "joi";
 
-export const createContactSchema = Joi.object({});
+export const createContactSchema = Joi.object({
+  name: Joi.string().required().messages({
+    "any.required": `"name" is required`,
+    "string.empty": `"name" cannot be empty`,
+  }),
+  email: Joi.string().email().required().messages({
+    "any.required": `"email" is required`,
+    "string.email": `"email" must be a valid email`,
+    "string.empty": `"email" cannot be empty`,
+  }),
+  phone: Joi.string().required().messages({
+    "any.required": `"phone" is required`,
+    "string.empty": `"phone" cannot be empty`,
+  }),
+});
 
-export const updateContactSchema = Joi.object({});
+export const updateContactSchema = Joi.object({
+  name: Joi.string().messages({
+    "string.empty": `"name" cannot be empty`,
+  }),
+  email: Joi.string().email().messages({
+    "string.email": `"email" must be a valid email`,
+    "string.empty": `"email" cannot be empty`,
+  }),
+  phone: Joi.string().messages({
+    "string.empty": `"phone" cannot be empty`,
+  }),
+})
+  .min(1)
+  .messages({
+    "object.min": "Body must have at least one field to update",
+  });
