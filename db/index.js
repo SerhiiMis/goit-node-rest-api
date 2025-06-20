f93532be46fb4beae32b83485c34148d8d214c17
Serhii Mishovych <serhiimishovych@gmail.com>
2025-06-20 18:23:22 +0200

    Fix sequelize in db/index.js

--- db/index.js
+++ db/index.js
@@ -9,7 +9,13 @@ export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   port: DB_PORT,
   dialect: "postgres",
-  logging: false, // вимкнути SQL-логи
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 export const connectToDatabase = async () => {
