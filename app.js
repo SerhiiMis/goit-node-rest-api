47ccef40eeff766614b352a80928de5a0f3fa716
Serhii Mishovych <serhiimishovych@gmail.com>
2025-06-30 19:52:28 +0200

    Feat: add static file serving for avatars

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const startServer = async () => {
   app.use(morgan("tiny"));
   app.use(cors());
   app.use(express.json());
+  app.use(express.static("public"));
 
   app.use("/api/contacts", contactsRouter);
   app.use("/api/auth", authRouter);
